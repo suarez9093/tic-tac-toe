480edce379ffd7ca6e4c6ea855f5d46bda828d1e
suarez9093 <suarez9093@hotmail.com>
2020-08-09 09:20:42 -0600

    X's and O's appearing onClick. Next step is to create biz logic to handle if user wins or not

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Square({ value }) {
+function Square({ value, onClick }) {
   return (
-    <button className="square" type="button">
+    <button className="square" type="button" onClick={onClick}>
       {value}
     </button>
   );
