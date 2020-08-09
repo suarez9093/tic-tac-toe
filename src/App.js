e0d9a53d1f09efb533596e45e045296e63d9db5c
suarez9093 <suarez9093@hotmail.com>
2020-08-08 22:55:52 -0600

    added scss to grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Board from './components/Board';
+import './styles/root.scss';
 
 const App = () => (
-  <>
+  <div className="app">
     <h1>Tic Tac Toe</h1>
     <Board />
-  </>
+  </div>
 );
 
 export default App;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Square({ value }) {
-  return <button type="button">{value}</button>;
+  return (
+    <button className="square" type="button">
+      {value}
+    </button>
+  );
 }
 
 export default Square;
