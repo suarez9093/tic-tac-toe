c6e7e8b94420270c62bef6251c755f5926dfe39a
suarez9093 <suarez9093@hotmail.com>
2020-08-10 10:07:28 -0600

    styled the entire app

--- src/components/StatusMessage.js
+++ src/components/StatusMessage.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
-function StatusMessage({ winner, current }) {
-  const noMovesNext = current.board.every(el => el !== null);
+const StatusMessage = ({ winner, current }) => {
+  const noMovesLeft = current.board.every(el => el !== null);
+
   return (
-    <h2>
-      {winner && winner`Winner is ${winner}`}
-      {!winner &&
-        !noMovesNext &&
-        `Next player is ${current.isXNext ? 'X' : 'O'}`}
-      {!winner && noMovesNext && 'X and O Tied'}
-    </h2>
+    <div className="status-message">
+      {winner && (
+        <>
+          Winner is{' '}
+          <span className={winner === 'X' ? 'text-green' : 'text-orange'}>
+            {winner}
+          </span>
+        </>
+      )}
+      {!winner && !noMovesLeft && (
+        <>
+          Next player is{' '}
+          <span className={current.isXNext ? 'text-green' : 'text-orange'}>
+            {current.isXNext ? 'X' : 'O'}{' '}
+          </span>
+        </>
+      )}
+      {!winner && noMovesLeft && (
+        <>
+          <span className="text-green">X</span> and{' '}
+          <span className="text-orange">O</span> tied
+        </>
+      )}
+    </div>
   );
-}
+};
 
 export default StatusMessage;
--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-function calculateWinner(squares) {
+export function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,10 +12,14 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        winningSquares: [a, b, c],
+      };
     }
   }
-  return null;
+  return {
+    winner: null,
+    winningSquares: [],
+  };
 }
-
-export { calculateWinner };
